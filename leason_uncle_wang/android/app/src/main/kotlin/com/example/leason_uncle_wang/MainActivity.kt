e6922095cca0588bd425a7b66d3b5159dce3f291
Marvel <marvel@gmail.com>
2024-03-05 16:33:17 +0800

    1

--- leason_uncle_wang/android/app/src/main/kotlin/com/example/leason_uncle_wang/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.leason_uncle_wang
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lesson_uncle_wang/android/app/src/main/kotlin/com/example/lesson_uncle_wang/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lesson_uncle_wang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
